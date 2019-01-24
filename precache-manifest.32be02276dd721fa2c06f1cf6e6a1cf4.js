d0c567f3443235ad15aac798bf59e62ffdc05c83
Oliver Grack <oliver.grack@gmail.com>
2019-01-24 10:40:58 +0100

    Updates

---
+++ precache-manifest.32be02276dd721fa2c06f1cf6e6a1cf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f24f18f1026e412bce29",
+    "url": "/static/js/main.f24f18f1.chunk.js"
+  },
+  {
+    "revision": "924c13c00d1d1ce8d9a4",
+    "url": "/static/js/1.924c13c0.chunk.js"
+  },
+  {
+    "revision": "f24f18f1026e412bce29",
+    "url": "/static/css/main.361d8528.chunk.css"
+  },
+  {
+    "revision": "826d385987344490e49cc0f09c480fdb",
+    "url": "/index.html"
+  }
+];
